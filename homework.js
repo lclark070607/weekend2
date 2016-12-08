1e4a42f5bbc8615055e4b5f79e1e37d64adf40d7
Louise Clark <lclark070607@gmail.com>
2016-12-08 15:27:04 -0500

    Function 1 and 2

---
+++ homework.js
@@ -0,0 +1,33 @@
+console.log('#1 Sum');
+
+//Write a function called sum that 
+//takes two parameters and returns the sum of those 2 numbers.
+
+//English version
+
+function Sum(one, two) {
+    console.log(one + two)
+    return one + two;
+}
+    Sum(2, 6);
+    Sum(9.42375, 5.3275)
+
+console.log('#2 Avg');
+
+//Write a function named avg 
+//that takes 3 parameters and returns the average of those 3 numbers.
+
+// Order of Operations:  modulus, mult/div, add/subtraction
+//Parentheses can reorder anything
+
+function avg(one, two, three) {
+    console.log((one + two + three) / 3);
+    return ((one + two + three) / 3);
+    }
+        avg(4, 4, 4);
+        avg(5, 6, 1);
+      
+        
+
+      
+    
